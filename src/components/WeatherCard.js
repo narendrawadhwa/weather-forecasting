4760c3295c0bdc6452901e44ce9decbe1480da71
narendrawadhwa <bakemeawish1201@gmail.com>
2024-01-13 02:51:30 +0530

    added arrow down up

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import config from '../config';
 import LoadingComponent from './Loading';
-import { BsExclamationTriangle } from 'react-icons/bs';
+import { BsArrowDown, BsArrowUp, BsExclamationTriangle } from 'react-icons/bs';
 import { humidity } from '../assets';
 
 
@@ -19,31 +19,32 @@ const WeatherCard = ({ lat, lon, city }) => {
       try {
         let apiUrl;
         let forecastApiUrl;
-  
+
         if (city) {
           apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
           forecastApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
         } else if (lat && lon) {
           apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
           forecastApiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-        } 
-  
+        }
+
         if (apiUrl) {
           setLoading(true);
-  
+
           // Fetching current weather data
           const weatherResponse = await axios.get(apiUrl);
-  
-        
-  
+
+
+
           setWeatherData(weatherResponse.data);
           setLoading(false);
           setError(null);
 
-  
+
           // Fetching 5-day forecast data
           const forecastResponse = await axios.get(forecastApiUrl);
-          setForecastData(forecastResponse.data);
+          const dailyForecastData = extractDailyForecast(forecastResponse.data.list);
+          setForecastData(dailyForecastData);
         }
       } catch (error) {
         console.error(error);
@@ -51,10 +52,10 @@ const WeatherCard = ({ lat, lon, city }) => {
         setLoading(false);
       }
     };
-  
+
     fetchData();
   }, [lat, lon, city, apiKey]);
-  
+
 
   const kelvinToCelsius = (kelvin) => Math.round(kelvin - 273.15);
   const metersPerSecondToMilesPerHour = (metersPerSecond) => (metersPerSecond * 2.23694).toFixed(2);
@@ -75,9 +76,38 @@ const WeatherCard = ({ lat, lon, city }) => {
   };
 
   const getWeatherIcon = (iconCode) => {
-    return  `https://openweathermap.org/img/wn/${iconCode}.png`;
+    return `https://openweathermap.org/img/wn/${iconCode}.png`;
   };
+  // Function to extract daily forecast data (minimum and maximum temperatures)
+  const extractDailyForecast = (forecastList) => {
+    const dailyForecastData = {};
 
+    forecastList.forEach((item) => {
+      const date = new Date(item.dt * 1000);
+      const dayKey = date.toDateString();
+
+      if (!dailyForecastData[dayKey]) {
+        dailyForecastData[dayKey] = {
+          date: date,
+          temp_min: item.main.temp_min,
+          temp_max: item.main.temp_max,
+          humidity: item.main.humidity,
+        };
+      } else {
+        // Update min and max temperatures if a more recent reading is found
+        dailyForecastData[dayKey].temp_min = Math.min(
+          dailyForecastData[dayKey].temp_min,
+          item.main.temp_min
+        );
+        dailyForecastData[dayKey].temp_max = Math.max(
+          dailyForecastData[dayKey].temp_max,
+          item.main.temp_max
+        );
+      }
+    });
+
+    return Object.values(dailyForecastData);
+  };
   return (
     <div className='mt-5'>
       {loading && <LoadingComponent />}
@@ -89,20 +119,20 @@ const WeatherCard = ({ lat, lon, city }) => {
       )}
 
       {weatherData && !loading && !error && (
-        
+
         <div className='text-white justify-content-between flex lg:flex-row flex-col'>
-          <span className='flex justify-between'>
-          <span className='text-left'>
-            <img src={getWeatherIcon(weatherData.weather[0].icon)} alt='Weather Icon' className='rounded-full bg-white w-14 h-14' />
-            <p className='fs-6 mt-1 mb-0'>{weatherData.name}, {weatherData.sys.country}</p>
-            <h2 style={{ fontSize: '60px' }} className='mb-1 mt-0'>{kelvinToCelsius(weatherData.main.temp)}°C</h2>
-            <p className='fs-6 m-0 mb-2 fw-semibold'>{weatherData.weather[0].main}</p>
-            
-          </span>
-          <span className='text-right lg:hidden block'>
-            <p className='fs-6 m-0'>{getAbbreviatedDay(currentDate)}, {formatAMPM(currentDate)}</p>
-            <p className='fs-6 m-0'>{currentDate.toLocaleDateString(undefined, { day: 'numeric', month: 'short', year: 'numeric' })}</p>
-          </span>
+          <span className='flex justify-between min-w-[20%]'>
+            <span className='text-left'>
+              <img src={getWeatherIcon(weatherData.weather[0].icon)} alt='Weather Icon' className='rounded-full bg-white w-14 h-14' />
+              <p className='fs-6 mt-1 mb-0'>{weatherData.name}, {weatherData.sys.country}</p>
+              <h2 style={{ fontSize: '60px' }} className='mb-1 mt-0'>{kelvinToCelsius(weatherData.main.temp)}°C</h2>
+              <p className='fs-6 m-0 mb-2 fw-semibold'>{weatherData.weather[0].main}</p>
+
+            </span>
+            <span className='text-right lg:hidden block'>
+              <p className='fs-6 m-0'>{getAbbreviatedDay(currentDate)}, {formatAMPM(currentDate)}</p>
+              <p className='fs-6 m-0'>{currentDate.toLocaleDateString(undefined, { day: 'numeric', month: 'short', year: 'numeric' })}</p>
+            </span>
           </span>
 
 
@@ -117,7 +147,7 @@ const WeatherCard = ({ lat, lon, city }) => {
             </span>
             <span>
               <p className='details-heading'>Wind</p>
-              <h3 className='details-content'>{metersPerSecondToMilesPerHour(weatherData.wind.speed)}mp/h</h3>
+              <h3 className='details-content'>{metersPerSecondToMilesPerHour(weatherData.wind.speed)} mp/h</h3>
             </span>
           </div>
 
@@ -125,55 +155,67 @@ const WeatherCard = ({ lat, lon, city }) => {
           {forecastData && (
             <div className='my-4 flex justify-between flex-col items-center'>
               <>
-              <h3 className='text-center'>5-Days Forecast:</h3>
-              <span className='flex flex-row flex-wrap justify-center items-center'>
-              {forecastData.list.slice(0, 5).map((day, index) => (
-                <div key={index} className='border-2 border-white gap-2 bg-white bg-opacity-25 rounded-2 m-1 p-2 min-w-[150px] flex justify-center items-center'>
-                  <p>{getAbbreviatedDay(new Date(day.dt * 1000))} </p>
+                <h3 className='text-center'>5-Days Forecast</h3>
+                <span className='flex flex-row flex-wrap justify-center items-center'>
+                  {forecastData.map((day, index) => (
+                    <div
+                      key={index}
+                      className='border-2 border-white gap-4 bg-white bg-opacity-25 rounded-2 m-1 p-2 w-[270px] flex justify-center items-center'
+                    >
+                      <p>
+                        {getAbbreviatedDay(day.date)}{' '}
+                      </p>
 
-                  <span className='flex flex-row justify-center items-start'>
-                  <img src={humidity} alt='Humidity Icon' className='w-3 h-3 mr-1 mt-2' />            
-                  <p className=''>{weatherData.main.humidity}</p>
-                  </span>
+                      <span className='flex flex-row justify-center items-start'>
+                        <img
+                          src={humidity}
+                          alt='Humidity Icon'
+                          className='w-3 h-3 mr-1 mt-2'
+                        />
+                        <p className=''>{day.humidity}</p>
+                      </span>
+                      <p className='flex flex-row'>
+                        {kelvinToCelsius(day.temp_min)}°
+                        <BsArrowDown className='text-white w-3 mr-1 mt-1' />
+                        {kelvinToCelsius(day.temp_max)}°
+                        <BsArrowUp className='text-white mt-1 w-3' />
+                      </p>
 
-                  <p>
-                  {kelvinToCelsius(day.main.temp_min)}° {" "}{kelvinToCelsius(day.main.temp_max)}°
-                    </p>
-                </div>
-              ))}
-              </span>
+                    </div>
+                  ))}
+                </span>
               </>
-           
+
               <div className={`justify-between hidden lg:flex w-[50%] absolute bottom-5 bg-white mx-auto bg-opacity-25 rounded-3 p-3 border border-white`} >
-            <span>
-              <p className='details-heading'>Feels Like</p>
-              <h3 className='details-content'>{kelvinToCelsius(weatherData.main.feels_like)}°C</h3>
-            </span>
-            <span>
-              <p className='details-heading'>Humidity</p>
-              <h3 className='details-content'>{weatherData.main.humidity}%</h3>
-            </span>
-            <span>
-              <p className='details-heading'>Wind</p>
-              <h3 className='details-content'>{metersPerSecondToMilesPerHour(weatherData.wind.speed)}mph</h3>
-            </span>
-            <span>
-              <p className='details-heading'>Pressure</p>
-              <h3 className='details-content'>{weatherData.main.pressure} mb</h3>
-            </span>
-            <span>
-              <p className='details-heading'>Visibility</p>
-              <h3 className='details-content'>{weatherData.visibility/1000} km</h3>
-            </span>
-          </div>
+                <span>
+                  <p className='details-heading'>Feels Like</p>
+                  <h3 className='details-content'>{kelvinToCelsius(weatherData.main.feels_like)}°C</h3>
+                </span>
+                <span>
+                  <p className='details-heading'>Humidity</p>
+                  <h3 className='details-content'>{weatherData.main.humidity}%</h3>
+                </span>
+                <span>
+                  <p className='details-heading'>Wind</p>
+                  <h3 className='details-content'>{metersPerSecondToMilesPerHour(weatherData.wind.speed)} mph</h3>
+                </span>
+                <span>
+                  <p className='details-heading'>Pressure</p>
+                  <h3 className='details-content'>{weatherData.main.pressure} mb</h3>
+                </span>
+                <span>
+                  <p className='details-heading'>Visibility</p>
+                  <h3 className='details-content'>{weatherData.visibility / 1000} km</h3>
+                </span>
+              </div>
             </div>
           )}
-          <span className='text-right hidden lg:block'>
+          <span className='text-right hidden lg:block w-[25%]'>
             <p className='fs-6 m-0'>{getAbbreviatedDay(currentDate)}, {formatAMPM(currentDate)}</p>
             <p className='fs-6 m-0'>{currentDate.toLocaleDateString(undefined, { day: 'numeric', month: 'short', year: 'numeric' })}</p>
           </span>
-          
-        
+
+
         </div>
       )}
     </div>
